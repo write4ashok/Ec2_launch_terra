abc5b3b562c14c26ddfa7c93f938a5e9320a627d
write4ashok <86650978+write4ashok@users.noreply.github.com>
2024-05-07 18:30:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,8 @@
-# main.tf
-
-# Define provider
 provider "aws" {
-  region = "ap-south-1"  # Specify your desired region
+  region = "ap-south-1" # Change to your desired AWS region
 }
 
-# Define EC2 instance
 resource "aws_instance" "example" {
-  ami           = "ami-05295b6e6c790593e"  # Specify the AMI ID for your desired OS
-  instance_type = "t2.micro"               # Specify the instance type
-  key_name      = "jenkinspipe.pem"     # Specify the key pair name to access the instance
-}
-
-# Output the public IP address of the instance
-output "instance_public_ip" {
-  value = aws_instance.example.public_ip
+  ami           = "ami-0f58b397bc5c1f2e8" # Change to your desired AMI ID
+  instance_type = "t2.micro"     # Change to your desired instance type
 }
